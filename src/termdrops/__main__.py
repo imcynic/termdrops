4e4bc9f50d781d11f6bd3227ccc9a649e985af25
imcynic <banner94@outlook.com>
2025-02-17 04:32:44 -0600

    Update and rename main.py to __main__.py

---
+++ src/termdrops/__main__.py
@@ -0,0 +1,5 @@
+"""Main entry point for termdrops package."""
+from termdrops.cli import main
+
+if __name__ == '__main__':
+    main()
--- src/termdrops/main.py
+++
@@ -1,5 +0,0 @@
-"""Main entry point for the termdrops CLI."""
-from termdrops.cli import main
-
-if name == 'main':
-    main()
